6553df25a86a5caa9cb06763fdeeefee1fe3ec8d
i0r52 <106861324+i0r52@users.noreply.github.com>
2022-06-05 22:32:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,14 @@ def gettoken(refresh_token):
           'client_secret':secret,
           'redirect_uri':'http://localhost:53682/'
          }
+    
+    data={'grant_type': 'refresh_token',
+          'refresh_token': "0.AX0Ah-A6iYqzLU6HwimXSjrKDLE_H8qCNEJHq_2-lqVc4VWaAMc.AgABAAEAAAD--DLA3VO7QrddgJg7WevrAgDs_wQA9P_GKoED7vZ14dMqEgs4urqmUcHLIJ06wAdMpvBfs0tiGRKK4n5kcXMk1-6vujzZ6pSE4kt2yEr7bI8VfVbEMi5F7Xrq4zlJoYmyRvRmEm_1zWa7i8Gpx73BFltsZkTp1zpjXRm2gKb4eMr_-PG59GerJryUEQAtl7GJgcbJlXgeUltROvRhWcNpUMpB6yiVa0bqnJiJY9yihWBkSvyfLZYtvnHeEl74brIcurROxr0WIdqtAk3Z7Ms80js4o_HaKn2wiNH1ToaqpY5wtyNjhKw05VpRm2Mp8VVJ9cuBdQf-zyHVzF2JRaU6WZtbniAw5tUqZXTkDgo_1z9hhXaAwAgGNU9YWK-1qWzyTppdbjdbEdBVaxV5xggAt8dzxpgQMk0S0g9-GheVgestu667IOtTbTKKuhEEDGadueKbRi3WCki_KbBEi14AZEIOulajR37w7SYlG004Q065yLksBuEtMojjOxT3-6BCf1Af5mENC0ftE3-R8LHaVnzb9XHkyTZCYtNGDlnvW6nhOeZ9Zp2ejgrWR9tBCLO_2kk_lYg8KhgqJDMtrc9H3BRYAH5ZnCGbAyvm_Ho6Anw89K9E_d9cVy_A0Nvbx1fAZs8vHEGG23Fq1t52dl4tb4f71FdHbiX8Z_dJskHHyAEGYb2vpz9Uw1yz-jFY4DW5dhJpy0Uk9sZ9J-pVjHkDqvBhRWDJ0nZeL8BEWNMqLVdAbGtCetcnpHZesVtIzhoXc5L8oNsllxj2N_JlN_40kacwjYcSZ3GYtiv-cAjvRB-G7qdpLa5cOzVk6PuaYdlKvyHk",
+          'client_id':"ca1f3fb1-3482-4742-abfd-be96a55ce155",
+          'client_secret':"Wvh8Q~BHeYHyVVgE1dPW3fACpuKPGI.jWajabdes",
+          'redirect_uri':'http://localhost:53682/'
+         }
+    
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
     jsontxt = json.loads(html.text)
     refresh_token = jsontxt['refresh_token']
